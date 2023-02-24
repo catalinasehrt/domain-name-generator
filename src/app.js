b9038a4bad7d37ad02d8a1cab8fe48e87f8de459
catalinasehrt <catalina.dali94@gmail.com>
2023-02-24 15:31:35 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "puedes"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let domain = "";
+let extenciones = ["com", "es", "net", "us"];
+let tem = "";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      let random = GenerateRandom(extenciones);
+      tem = pronoun[i] + adj[j] + noun[k];
+      if (random == "es") {
+        domain += tem.substring(0, tem.length - 2) + ".es\n";
+      } else {
+        domain += tem + `.${random}\n`;
+      }
+    }
+  }
+}
+
+function GenerateRandom(extenciones) {
+  return extenciones[Math.floor(Math.random() * extenciones.length)];
+}
+
+console.log(domain);
